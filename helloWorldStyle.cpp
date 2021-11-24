bc0e5242dfad03e5aa5f8a375f7db9d79bd7f3f3
Ubuntu <ubuntu@ip-172-31-5-196.ec2.internal>
2021-11-24 05:52:06 +0000

    Created helloWorldStyle.cpp with 1 line of code

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,9 @@
+// Created by: Melody Berhane
+// Date: Nov. 23, 2021
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
